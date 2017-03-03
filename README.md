599fea2dcabe4fdd1b5382e5be21a9e4db8e8137
David Yee <dveyee@hotmail.com>
2017-03-03 12:41:11 -0700

    Implement assignment requirements

--- README.md
+++ README.md
@@ -12,4 +12,29 @@ Contributors / Licensing
 
 Generally everything is LICENSE'D under the Apache 2 license by Abram Hindle.
 
+Additions made for CMPUT 404 Winter 2017 by David Yee are similarly licensed 
+under the Apache 2 license. Additional resources and their respective licenses
+are cited below.
 
+Sources
+=======
+
+* Flask JSONify (http://flask.pocoo.org/docs/0.12/api/#flask.json.jsonify)
+
+* `XMLHTTPRequest` documentation by the Mozilla Developer Network
+  (https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/setRequestHeader)
+
+* Dictionary iteration idea by
+  sberry (http://stackoverflow.com/users/141555/sberry) from
+  http://stackoverflow.com/a/3294899/2557554 and licensed under
+  CC-BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/)
+
+* JSON response idea by
+  Torben (http://stackoverflow.com/users/398844/torben) from
+  http://stackoverflow.com/a/8416963/2557554 and licensed under
+  CC-BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/)
+
+* Circle formula and code idea by
+  Philipp Jahoda (http://stackoverflow.com/users/1590502/philipp-jahoda) at
+  http://stackoverflow.com/a/25929952/2557554 and licensed under
+  CC-BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/)
--- server.py
+++ server.py
@@ -21,9 +21,10 @@
 #     pip install flask
 
 
-import flask
-from flask import Flask, request
 import json
+
+from flask import Flask, request, url_for, redirect, jsonify
+
 app = Flask(__name__)
 app.debug = True
 
@@ -55,7 +56,7 @@ class World:
         return self.space
 
 # you can test your webservice from the commandline
-# curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
+# curl -v   -H "Content-Type: application/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}'
 
 myWorld = World()          
 
@@ -74,27 +75,33 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect(url_for('static', filename='index.html'))
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+
+    for key, value in data.iteritems():
+        myWorld.update(entity, key, value)
+
+    return jsonify(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return jsonify(myWorld.world())
 
 if __name__ == "__main__":
     app.run()
